14b5a5fe1f7defa00bdc290c054514965efb91b3
Murtaza Hussaini <69414945+hussaini021@users.noreply.github.com>
2026-02-14 20:13:41 +0430

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 import os
 import vpn2
+import os
+import sys
 git pull
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+os.chdir(BASE_DIR)
 
-vpn2.main()
+if __name__ == "__main__":
+    main()
